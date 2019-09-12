df9e9ed98d5333367fe838d7b871b2cc0c4825e1
Caleb Spain <crspain@uh.edu>
2019-09-12 18:02:39 -0500

    dynamic content

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,35 @@
 class PostsController < ApplicationController
   def index
+    @posts = [
+      {
+        :title => "CSS tricks",
+        :content => "This is the content area",
+        :slug => "css-tricks",
+        :img_url => "https://i.udemycdn.com/course/750x422/1561458_7f3b.jpg",
+        :featured => false
+      },
+      {
+        :title => "Top 10 Javascript Frameworks",
+        :content => "This is the content area",
+        :slug => "top-10-javascript-frameworks",
+        :img_url => "https://codersdiaries.com/wp-content/uploads/2017/10/Best-Javascript-frameworks.png",
+        :featured => false
+      },
+      {
+        :title => "Top 10 PHP Frameworks",
+        :content => "This is the content area",
+        :slug => "top-10-php-frameworks",
+        :img_url => "https://miro.medium.com/max/826/1*6wCpASLwb2B28E5qD2HQpA.png",
+        :featured => true
+      },
+      {
+        :title => "Top 10 Node JS Frameworks",
+        :content => "This is the content area",
+        :slug => "top-10-node-js-frameworks",
+        :img_url => "https://devtechnosys.com/insights/wp-content/uploads/2019/06/xTop-List-of-best-Nodejs-Frameworks-for-Developers.png.pagespeed.ic.oudPmHatJu.png",
+        :featured => false
+      }
+    ]
   end
 
   def show
--- db/migrate/20190908151956_create_posts.rb
+++ db/migrate/20190908151956_create_posts.rb
@@ -3,8 +3,10 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.string :title
       t.text :content
+      t.string :img_url
       t.string :slug
       t.integer :category_id
+      t.boolean :featured
 
       t.timestamps
     end
